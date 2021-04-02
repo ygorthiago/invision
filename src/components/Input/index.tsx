36f38a65c289e7f1a2e15397932609870838b0cd
jenova <ygorthiagoff@outlook.com>
2021-04-02 12:29:40 -0300

    feat: input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,43 @@
+import React, { InputHTMLAttributes, LegacyRef, useState } from 'react';
+
+import { Container, InputErrorMessage } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  inputName: string;
+  error?: boolean;
+  errorMessage?: string;
+  inputRef?: LegacyRef<HTMLInputElement> | undefined;
+}
+
+function Input({
+  label,
+  error,
+  errorMessage,
+  inputName,
+  inputRef,
+  ...rest
+}: InputProps) {
+  const [value, setValue] = useState('');
+
+  return (
+    <>
+      <Container isErrored={!!error}>
+        <input
+          name={inputName}
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          id="email"
+          ref={inputRef}
+          {...rest}
+        />
+        <label htmlFor="email" className="labelName">
+          <span className="contentName">{label}</span>
+        </label>
+      </Container>
+      {errorMessage && <InputErrorMessage>{errorMessage}</InputErrorMessage>}
+    </>
+  );
+}
+
+export default Input;
