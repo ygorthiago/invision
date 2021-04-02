1fe303113b7f319456628af7f8c855940a771da3
jenova <ygorthiagoff@outlook.com>
2021-04-02 12:29:19 -0300

    feat: global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  :root {
+  --white: #FFFFFF;
+
+  --grey-200: #d6d6d6;
+  --grey-500: #707070;
+
+  --black: #000000;
+
+  --green-mint: #A9C5BA;
+
+  --red: #c54040;
+}
+
+  body, input, button {
+    font-family: 'Mulish', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
